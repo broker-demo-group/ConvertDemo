ba04330cd00431f7ec20100b624b324232d7c364
CrystalCat <edgec2017@gmail.com>
2022-07-03 00:00:47 -0400

    Complete the register and login

--- src/main/java/com/brokerdemo/brokerconvertdemoproject/entity/User.java
+++ src/main/java/com/brokerdemo/brokerconvertdemoproject/entity/User.java
@@ -2,6 +2,7 @@ package com.brokerdemo.brokerconvertdemoproject.entity;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.Date;
