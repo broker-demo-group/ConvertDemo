640329f93403794e210b315e43235da631176ff4
CrystalCat <edgec2017@gmail.com>
2022-07-06 19:38:15 -0400

    add info for login~

---
+++ src/main/java/com/brokerdemo/brokerconvertdemoproject/controller/Dashboard.java
@@ -0,0 +1,21 @@
+package com.brokerdemo.brokerconvertdemoproject.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class Dashboard {
+    @RolesAllowed("ROLE_USER")
+    @GetMapping("/dashboard")
+    public Map<String,String> loginSuccess(Authentication authentication){
+        Map<String,String> result = new HashMap<>();
+        result.put("status","success");
+        result.put("user",authentication.getName());
+        return result;
+    }
+}
--- src/main/java/com/brokerdemo/brokerconvertdemoproject/controller/Login.java
+++
@@ -1,5 +0,0 @@
-package com.brokerdemo.brokerconvertdemoproject.controller;
-
-public class Login {
-
-}
